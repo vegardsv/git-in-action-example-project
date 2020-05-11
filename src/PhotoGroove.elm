c3fcc0224e335b15149bb6464dca08e8efef9813
Vegard Svendsen <vegardsvendsen@Vegards-MacBook-Pro-2.local>
2020-05-11 20:13:33 +0200

    first commit

---
+++ src/PhotoGroove.elm
@@ -0,0 +1,40 @@
+module PhotoGroove exposing (main)
+
+import Html exposing (div, h1, img, text)
+import Html.Attributes exposing (..)
+
+
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+viewThumbnail selectedUrl thumb =
+    img [ src (urlPrefix ++ thumb.url), classList [ ( "selected", selectedUrl == thumb.url ) ] ]
+        []
+
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnails" ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
+
+
+main =
+    view initialModel
